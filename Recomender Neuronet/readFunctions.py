2a736e2b1d36f8af0abbfce8880138ccef7df025
Emilikan <emilqiwi@gmail.com>
2019-11-25 23:09:58 +0300

    Text Tonality

---
+++ Recomender Neuronet/readFunctions.py
@@ -0,0 +1,31 @@
+import gzip
+import os.path
+import pandas as pd
+
+def parse(path):
+    g = gzip.open(path, 'rb')
+    for l in g:
+        yield eval(l)
+
+def openFile(path):
+    """
+    Unzip's the file and read into if reviews.csv doesn't exist
+    write to it so it will be faster to read in next time.
+       
+    :param: path (str) : path of .gz jason file
+    :returns: DataFrame of file contents
+    :rvalue: Pandas DataFrame
+    """
+    if os.path.isfile('reviews.csv') is False:
+        i = 0
+        df = {}
+        for d in parse(path):
+            df[i] = d
+            i += 1
+
+        df = pd.DataFrame.from_dict(df, orient='index')
+        df.to_csv('reviews.csv')
+    else:
+        df = pd.read_csv('reviews.csv')
+    
+    return df 
